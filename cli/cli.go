a045282f7e0d6d71f13e4ab8cef0c95f6b1d67ee
onao13 <onao13@outlook.com>
2024-08-04 01:03:20 +0300

    added cli create project

--- cli/cli.go
+++ cli/cli.go
@@ -1,14 +1,33 @@
 package main
 
+import (
+	"core-engine/cli/command"
+	"github.com/rs/zerolog/log"
+)
+
 type CLI struct {
+	IsDebug bool
 }
 
-func New() *CLI {
-	return &CLI{}
+func NewCLI(isDebug bool) *CLI {
+	return &CLI{
+		IsDebug: isDebug,
+	}
 }
 
-func (cli *CLI) Run() {
+func (cli *CLI) Run(args []string) int {
+	argLevel, curCommand := command.Select(args)
+	if curCommand == nil {
+		return 1
+	}
 
+	if cli.IsDebug {
+		log.Info().Msgf("Current Command: %s", curCommand.Name)
+	}
+
+	args = args[argLevel:]
+
+	return curCommand.Run(args)
 }
 
 func (cli *CLI) Usage() string {
--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module core-engine
 
 go 1.22.4
 
-require github.com/samber/lo v1.46.0
+require (
+	github.com/rs/zerolog v1.33.0
+	github.com/samber/lo v1.46.0
+)
 
-require golang.org/x/text v0.16.0 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
