a35e4f1c9b5a252030b0a1726809e903185c1493
onao_13 <desend33@bwg-io.site>
2024-08-03 22:16:55 +0300

    Added select command

---
+++ command/select_subcommand.go
@@ -0,0 +1,68 @@
+package command
+
+import (
+	"fmt"
+	"github.com/samber/lo"
+)
+
+func Select(args []string) int {
+	if len(args) == 0 {
+		return 1
+	}
+
+	baseCommand := selectCommand(args[0])
+	if baseCommand == nil {
+		return 1
+	}
+
+	if baseCommand.Subcommands == nil {
+		if baseCommand.Run == nil {
+			return 1
+		}
+		return baseCommand.Run(args[1:])
+	}
+
+	curCommand, argLevel := findSubcommandIntoArgs(baseCommand, args)
+
+	if curCommand == nil || curCommand.Run == nil {
+		return 1
+	}
+
+	// clear subcommand
+	args = args[argLevel:]
+
+	fmt.Println(curCommand.Name)
+
+	return curCommand.Run(args)
+}
+
+func findSubcommandIntoArgs(com *Command, args []string) (*Command, int) {
+	var (
+		curCommand *Command = com
+		argLevel            = 1
+	)
+
+	for _, arg := range args[1:] {
+		destSubcommand := selectSubcommandCommand(curCommand, arg)
+		if destSubcommand == nil {
+			break
+		}
+		curCommand = destSubcommand
+		argLevel += 1
+	}
+
+	return curCommand, argLevel
+}
+
+func selectSubcommandCommand(com *Command, name string) *Command {
+	if com == nil || com.Subcommands == nil {
+		return nil
+	}
+	baseSubcommand, ok := com.Subcommands[name]
+	return lo.Ternary(!ok || baseSubcommand == nil, nil, baseSubcommand)
+}
+
+func selectCommand(name string) *Command {
+	baseCommand, ok := Commands[name]
+	return lo.Ternary(!ok || baseCommand == nil, nil, baseCommand)
+}
