bf6b10a51cb90b4f9489abd30b62d7d845b42f34
onao_13 <desend33@bwg-io.site>
2024-08-03 22:15:34 +0300

    Added base flags, command and subcommand

---
+++ common/flags.go
@@ -0,0 +1,19 @@
+package common
+
+// Region initialization project flags
+
+const (
+	FlagProjectName = "project-name"
+	FlagLocation    = "location"
+)
+
+// End initialization project flags
+
+// Region parse file
+
+const (
+	FlagParseFile  = "file"
+	FlagParseFiles = "files"
+)
+
+// Region parse files
---
+++ common/subcommand.go
@@ -0,0 +1,26 @@
+package common
+
+// Region main subcommand
+
+const (
+	SubcommandProject = "project"
+	SubcommandEngine  = "engine"
+)
+
+// End main subcommand
+
+// Region subcommand project
+
+const (
+	SubcommandProjectCreate = "create"
+)
+
+// End subcommand project
+
+// Region subcommand engine
+
+const (
+	SubcommandEngineParse = "parse"
+)
+
+// End subcommand engine
