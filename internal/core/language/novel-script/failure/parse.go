ee2dda3b17866065bb45a37a7a91bd58c4ae1e62
onao13 <onao13@outlook.com>
2024-08-04 21:40:17 +0300

    removed errors

--- internal/core/language/novel-script/failure/parse.go
+++ internal/core/language/novel-script/failure/parse.go
@@ -3,32 +3,13 @@ package failure
 import "errors"
 
 var (
-	ErrNoChapterName = errors.New("no chapter name")
-	ErrNoActName     = errors.New("no act name")
+	ErrPersonAlreadyExist = errors.New("person already exist")
+	ErrPersonNotFound     = errors.New("person not found")
+	ErrEnsAlreadyExist    = errors.New("ens already exist")
 
-	ErrNoPerson      = errors.New("no person")
-	ErrNoPersonName  = errors.New("no person name")
-	ErrNoPersonAsset = errors.New("no person asset")
+	ErrNoEnsFile = errors.New("no ens file")
 
-	ErrNoPersonReplica = errors.New("no person replica")
-
-	ErrUnsupportedAssetFormat = errors.New("unsupported asset format")
-
-	ErrParseBackgroundName = errors.New("no background name")
-
-	ErrParseMusicName = errors.New("no music name")
-
-	ErrParseUseType    = errors.New("no use type")
-	ErrUseTypeNotFound = errors.New("use type not found")
-
-	ErrNoEnsName      = errors.New("no ens name")
-	ErrParseEnsSyntax = errors.New("parse ens syntax error. format <name> \"<file>\"")
-	ErrNoEnsFile      = errors.New("no ens file")
-
-	ErrNoSetName         = errors.New("no set name")
-	ErrEnsNotLoaded      = errors.New("ens not loaded")
 	ErrParseUseEnsSyntax = errors.New("parse use ens syntax error. format <ens-name>.<key> = <value>")
-	ErrSetTypeNotFound   = errors.New("set type not found")
 
 	ErrSelectSyntaxError = errors.New("select syntax error. format: select <var-name> = \"select1\" | \"select2\"")
 )
